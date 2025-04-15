fb5d1516813ad8cc3d4dcbc3a278c32f4fef39a9
Sargam Vyas <sargam.shukla12@gmail.com>
2025-04-15 00:14:06 -0700

    Adding tabs using js and css file

---
+++ tabs.js
@@ -0,0 +1,22 @@
+function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Open default tab on page load
+  window.onload = function() {
+    document.getElementById("defaultOpen").click();
+  };
+  
